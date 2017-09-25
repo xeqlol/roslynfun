afc4cee3592b5fe9c945e9683b75d139e4fb315d
Dmitry Nemkov <xeqlol@gmail.com>
2017-09-25 22:19:40 +0500

    asd

--- RoslynFun/RoslynFun/CodeFixProvider.cs
+++ RoslynFun/RoslynFun/CodeFixProvider.cs
@@ -27,22 +27,18 @@ namespace RoslynFun
 
         public sealed override FixAllProvider GetFixAllProvider()
         {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
             return WellKnownFixAllProviders.BatchFixer;
         }
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
-
-            // Find the type declaration identified by the diagnostic.
+            
             var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
-
-            // Register a code action that will invoke the fix.
+            
             context.RegisterCodeFix(
                 CodeAction.Create(
                     title: title,
